efd67fcb7e76b04926a8df818d6c090cd239de16
Lucas Dröse Ribeiro <lucasdroseribeiro@gmail.com>
2013-04-19 11:33:31 -0300

    commit do ciclo 1
    Alunos responsáveis: Rafael Zurawski
    Lucas Dröse Ribeiro

---
+++ Lotu - Sistema 1 º Ciclo/src/Sistema/Main.java
@@ -0,0 +1,29 @@
+package Sistema;
+import Classes.*;
+import Telas.*;
+
+public class Main {
+        /*
+         * 
+         * @author Rafael Zurawski
+         * 
+         */
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+        {
+		// TODO Auto-generated method stub
+
+		Cartao card = new Cartao();
+
+		System.out.println("teste");
+		System.out.println(card.getValidade());
+                
+                TelaLogin login = new TelaLogin();
+                login.setVisible(true);
+
+	}
+
+}
