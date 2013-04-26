988a7f1c94f73a6a1ddee83f2e77a447ef44f8a7
Lucas Dröse Ribeiro <lucasdroseribeiro@gmail.com>
2013-04-26 08:30:20 -0300

    commit do ciclo 2
    Alunos: Rafael Zurawski
    Lucas Ribeiro

---
+++ Lotu - Sistema Ciclo 2/src/Testes/UsuariosTest.java
@@ -0,0 +1,91 @@
+package Testes;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import Classes.Usuarios;
+
+public class UsuariosTest {
+
+        /*
+         * 
+         * @author Lucas Ribeiro
+         * 
+         * 
+         */
+
+
+	@Test
+	public final void testGetCPF() {
+		Usuarios usu = new Usuarios();
+		assertEquals("111.111.111-11",usu.getCPF());
+	}
+
+	public final void testGetRG() {
+		Usuarios usu = new Usuarios();
+		assertEquals("8102592543",usu.getRG());
+	}
+
+	@Test
+	public final void testGetNome() {
+		Usuarios usu = new Usuarios();
+		assertEquals("Rafael",usu.getNome());
+	}
+
+	@Test
+	public final void testGetCidade() {
+		Usuarios usu = new Usuarios();
+		assertEquals("Porto Alegre",usu.getCidade());
+	}
+
+	@Test
+	public final void testGetUF() {
+		Usuarios usu = new Usuarios();
+		assertEquals("RS",usu.getUF());
+	}
+
+
+	@Test
+	public final void testGetCEP() {
+		Usuarios usu = new Usuarios();
+		assertEquals(91250450,usu.getCEP());
+	}
+
+	@Test
+	public final void testGetPais() {
+		Usuarios usu = new Usuarios();
+		assertEquals("Brasil",usu.getPais());
+	}
+
+	@Test
+	public final void testGetNome_Mae() {
+		Usuarios usu = new Usuarios();
+		assertEquals("Minha mae",usu.getNome_Mae());
+	}
+
+	@Test
+	public final void testGetBairro() {
+		Usuarios usu = new Usuarios();
+		assertEquals("Cidade Baixa",usu.getBairro());
+	}
+
+	@Test
+	public final void testGetRua() {
+		Usuarios usu = new Usuarios();
+		assertEquals("Av. Coronel Genu�no",usu.getRua());
+	}
+
+	@Test
+	public final void testGetEmail() {
+		Usuarios usu = new Usuarios();
+		assertEquals("atendimento@lotuintegracao.com.br",usu.getEmail());
+	}
+
+	@Test
+	public final void testGetSenha() {
+		Usuarios usu = new Usuarios();
+		assertEquals("lotu",usu.getSenha());
+	}
+
+}
